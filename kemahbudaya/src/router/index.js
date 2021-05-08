ec4257c55d7cd174b397a8b231192de45a517976
Clalaves <daffaraihanzaki@gmail.com>
2021-05-08 22:45:39 +0700

    delete about on index.js

--- kemahbudaya/src/router/index.js
+++ kemahbudaya/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -16,11 +15,6 @@ const routes = [{
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
 ]
 
 const router = new VueRouter({
