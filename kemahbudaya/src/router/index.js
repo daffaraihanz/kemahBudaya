d5c6f483ba0e895da569e93d3f1286e689ac2c8b
Clalaves <daffaraihanzaki@gmail.com>
2021-05-08 22:43:26 +0700

    add router

---
+++ kemahbudaya/src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
