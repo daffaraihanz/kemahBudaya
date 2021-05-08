a1c009273ec9f71a1716ed321346ff66bb68649d
Clalaves <daffaraihanzaki@gmail.com>
2021-05-08 22:43:43 +0700

    add screen home

---
+++ kemahbudaya/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="row">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex laborum culpa deleniti in aspernatur, quisquam accusamus quaerat odit eaque obcaecati. Officiis omnis molestias tenetur excepturi?</p>
+        <button class="btn btn-danger">
+          <b>BTN DANGER</b>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
