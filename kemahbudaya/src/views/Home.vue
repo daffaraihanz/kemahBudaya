a76fe2f0d5bc61b9f0cc86892fbe610aa9c6c402
Clalaves <daffaraihanzaki@gmail.com>
2021-05-10 14:45:19 +0700

    add screen home

--- kemahbudaya/src/views/Home.vue
+++ kemahbudaya/src/views/Home.vue
@@ -1,19 +1,82 @@
 <template>
-  <div class="home">
+  <section class="home">
     <div class="container">
-      <div class="row">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex laborum culpa deleniti in aspernatur, quisquam accusamus quaerat odit eaque obcaecati. Officiis omnis molestias tenetur excepturi?</p>
-        <button class="btn btn-danger">
-          <b>BTN DANGER</b>
-        </button>
+      <div class="greeting mt-4">
+        <h2>Halo</h2>
+        <h2>Daffa Raihan Z</h2>
+      </div>
+      <div>
+        <h4 class="mt-4">Mari berwisata di tempat ini</h4>
+        <div v-for="myListMainCategory in list" :key="myListMainCategory.id" class="row">
+          <div class="col-lg-12">
+            <MainCategory bgcolor="#000" :myListMainCategory="myListMainCategory" />
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
+import MainCategory from "../components/MainCategory";
+
 export default {
   name: "Home",
-  components: {}
+  components: { MainCategory },
+  methods: {},
+  data: function() {
+    return {
+      list: [
+        {
+          id: 1,
+          title: "Taman Reptil"
+        },
+        {
+          id: 2,
+          title: "Kebun Bibit"
+        },
+        {
+          id: 3,
+          title: "Edukasi Pertanian"
+        },
+        {
+          id: 4,
+          title: "Demplot"
+        },
+        {
+          id: 5,
+          title: "Omah Budaya"
+        },
+        {
+          id: 6,
+          title: "Panembahan"
+        }
+      ]
+    };
+  }
 };
 </script>
+
+<style scoped>
+.home {
+  padding-bottom: 100px;
+}
+
+.greeting h2:nth-child(1) {
+  color: #737295;
+  font-family: "Montserrat";
+  font-size: 24px;
+}
+
+.greeting h2:nth-child(2) {
+  color: #100f37;
+  font-weight: 700;
+  font-size: 24px;
+}
+
+.home h4 {
+  color: #737295;
+  font-family: "Montserrat";
+  font-size: 18px;
+}
+</style>
