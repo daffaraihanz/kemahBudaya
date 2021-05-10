10b000c09716ce353dc6877230a601de54399a2d
Clalaves <daffaraihanzaki@gmail.com>
2021-05-10 14:45:14 +0700

    add css global

---
+++ kemahbudaya/src/views/Global.vue
@@ -0,0 +1,24 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+export default {
+  components: "Global"
+};
+</script>
+
+<style >
+section {
+  margin: 0 17px !important;
+  font-family: "Nunito";
+}
+
+html {
+  height: 100% !important;
+}
+
+body {
+  background: #eff4fc !important;
+}
+</style>
