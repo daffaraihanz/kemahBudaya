746e42eaf27409fb3ebd3f6a275ceb4b416f6567
Clalaves <daffaraihanzaki@gmail.com>
2021-06-26 11:52:01 +0700

    remove script nonaktif desktop

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -37,44 +37,44 @@ export default {
   mounted() {
     this.cekSesi();
     this.cek_koneksi_internet();
-    this.cek_desktop_atau_hp();
+    // this.cek_desktop_atau_hp();
   },
   methods: {
-    cek_desktop_atau_hp() {
-      var isMobile = {
-        Android: function() {
-          return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function() {
-          return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function() {
-          return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function() {
-          return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function() {
-          return (
-            navigator.userAgent.match(/IEMobile/i) ||
-            navigator.userAgent.match(/WPDesktop/i)
-          );
-        },
-        any: function() {
-          return (
-            isMobile.Android() ||
-            isMobile.BlackBerry() ||
-            isMobile.iOS() ||
-            isMobile.Opera() ||
-            isMobile.Windows()
-          );
-        }
-      };
+    // cek_desktop_atau_hp() {
+    //   var isMobile = {
+    //     Android: function() {
+    //       return navigator.userAgent.match(/Android/i);
+    //     },
+    //     BlackBerry: function() {
+    //       return navigator.userAgent.match(/BlackBerry/i);
+    //     },
+    //     iOS: function() {
+    //       return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    //     },
+    //     Opera: function() {
+    //       return navigator.userAgent.match(/Opera Mini/i);
+    //     },
+    //     Windows: function() {
+    //       return (
+    //         navigator.userAgent.match(/IEMobile/i) ||
+    //         navigator.userAgent.match(/WPDesktop/i)
+    //       );
+    //     },
+    //     any: function() {
+    //       return (
+    //         isMobile.Android() ||
+    //         isMobile.BlackBerry() ||
+    //         isMobile.iOS() ||
+    //         isMobile.Opera() ||
+    //         isMobile.Windows()
+    //       );
+    //     }
+    //   };
 
-      if (!isMobile.any()) {
-        this.$router.push("/desktop_not_allowed");
-      }
-    },
+    //   if (!isMobile.any()) {
+    //     this.$router.push("/desktop_not_allowed");
+    //   }
+    // },
 
     cek_koneksi_internet() {
       if (!navigator.onLine) {
--- src/views/desktop_not_allowed.vue
+++ src/views/desktop_not_allowed.vue
@@ -1,26 +1,26 @@
 <template>
-    <div style="height: 100vh" class="container w-100 d-flex text-center align-items-center justify-content-center">
-        <div class="text-center">
-            <img src="@/assets/WARNING.png" style="margin-top: -9rem; margin-bottom: -3rem" width="100%">
+  <div
+    style="height: 100vh"
+    class="container w-100 d-flex text-center align-items-center justify-content-center"
+  >
+    <div class="text-center">
+      <img src="@/assets/WARNING.png" style="margin-top: -9rem; margin-bottom: -3rem" width="100%" />
 
-            <h3>Oops!</h3>
-            <h3>Kamu hanya bisa mengakses aplikasi ini melalui ponsel</h3>
+      <h3>Oops!</h3>
+      <h3>Kamu hanya bisa mengakses aplikasi ini melalui ponsel</h3>
 
-            <br>
+      <br />
 
-            <router-link to="/home">
-                <button class="tombol-login" type="button">Kembali</button>
-            </router-link>
-
-        </div>
+      <router-link to="/home">
+        <button class="tombol-login" type="button">Kembali</button>
+      </router-link>
     </div>
+  </div>
 </template>
 
 
 <style scoped>
 .tombol-login {
-  /* margin-top: 40px; */
-  /* background-image: linear-gradient(#88b4fd, #619cfd); */
   color: white;
   font-weight: 700;
   font-size: 17px;
@@ -30,7 +30,7 @@
   border: none;
   border-bottom-style: solid;
   border-bottom-width: 6px;
-  background-color: #88B4FD;
-  border-bottom-color: #357DF4;
+  background-color: #88b4fd;
+  border-bottom-color: #357df4;
 }
 </style>
